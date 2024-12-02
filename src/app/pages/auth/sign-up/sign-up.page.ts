c8bd24d4cb587793a7a395400633a1208bc89e07
juan <juanpablomora36@gmail.com>
2024-12-02 19:08:03 -0300

    login_actualizado

--- src/app/pages/auth/sign-up/sign-up.page.ts
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -10,85 +10,98 @@ import { UtilsService } from 'src/app/services/utils.service';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   form = new FormGroup({
     uid: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    lastname: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
-    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
-  })
+    role: new FormControl(''),  // Deja el valor inicial vacío
+    subject: new FormControl({ value: '', disabled: true }),
+  });
 
-  firebaseSvc = inject(FirebaseService)
-  utilSvc =  inject(UtilsService)
+  firebaseSvc = inject(FirebaseService);
+  utilSvc = inject(UtilsService);
 
   ngOnInit() {
+    this.form.controls.role.valueChanges.subscribe(role => {
+      this.updateEmailDomain(role);
+      this.updateSubjectField(role);
+    });
   }
 
+  updateEmailDomain(role: string) {
+    const emailControl = this.form.controls.email;
+    if (role === 'estudiante') {
+      emailControl.setValue(emailControl.value.replace(/@profesor.duoc.cl$/, '') + '@duocuc.cl');
+    } else if (role === 'profesor') {
+      emailControl.setValue(emailControl.value.replace(/@duocuc.cl$/, '') + '@profesor.duoc.cl');
+    }
+  }
 
-  async submit(){
-    if(this.form.valid){
-      const loading =  await this.utilSvc.loading();
+  updateSubjectField(role: string) {
+    const subjectControl = this.form.controls.subject;
+    if (role === 'profesor') {
+      subjectControl.enable();
+    } else {
+      subjectControl.disable();
+    }
+  }
+
+  async submit() {
+    if (this.form.valid) {
+      const loading = await this.utilSvc.loading();
       await loading.present();
-    this.firebaseSvc.signUp(this.form.value as User).then(async res => {
 
-      await this.firebaseSvc.updateUser(this.form.value.name)
+      this.firebaseSvc.signUp(this.form.value as User).then(async res => {
+        await this.firebaseSvc.updateUser(this.form.value.name);
 
-      let uid = res.user.uid;
-      this.form.controls.uid.setValue(uid);
-      this.setUserInfo(uid)
+        let uid = res.user.uid;
+        this.form.controls.uid.setValue(uid);
+        this.setUserInfo(uid);
+        console.log(res);
+      }).catch(error => {
+        console.log(error);
+        this.utilSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline',
+        });
+      }).finally(() => {
+        loading.dismiss();
+      });
+    }
+  }
 
-      console.log(res);
+  async setUserInfo(uid: string) {
+    if (this.form.valid) {
+      const loading = await this.utilSvc.loading();
+      await loading.present();
 
+      let path = `users/${uid}`;
+      delete this.form.value.password;
 
-    }).catch( error => {
-      console.log(error);
-
-      this.utilSvc.presentToast({
-        message: error.message,
-        duration: 2500,
-        color: 'primary',
-        position: 'middle',
-        icon: 'alert-circle-outline'
-      })
-
-    }).finally (() => {
-      loading.dismiss();
-    })
-
+      this.firebaseSvc.setDocument(path, this.form.value).then(async res => {
+        this.utilSvc.saveInLocalStorage('user', this.form.value);
+        this.utilSvc.routerLink('/main/home');
+        this.form.reset();
+      }).catch(error => {
+        console.log(error);
+        this.utilSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline',
+        });
+      }).finally(() => {
+        loading.dismiss();
+      });
+    }
   }
 }
-async setUserInfo(uid: string){
-  if(this.form.valid){
-    const loading =  await this.utilSvc.loading();
-    await loading.present();
 
-    let path = 'users/${uid}';
-    delete this.form.value.password;
-
-  this.firebaseSvc.setDocument(path, this.form.value).then(async res => {
-
-    this.utilSvc.saveInLocalStorage('user', this.form.value)
-    this.utilSvc.routerLink('/main/home');
-    this.form.reset();
-
-
-  }).catch( error => {
-    console.log(error);
-
-    this.utilSvc.presentToast({
-      message: error.message,
-      duration: 2500,
-      color: 'primary',
-      position: 'middle',
-      icon: 'alert-circle-outline'
-    })
-
-  }).finally (() => {
-    loading.dismiss();
-  })
-
-}
-}
-}
