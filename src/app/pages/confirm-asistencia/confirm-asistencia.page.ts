6ed3e97aecc459a959545f6295bd10a59424c431
Schwarzesmak <mat.gonzalez6@duocuc.cl>
2024-12-15 21:32:06 -0300

    ta bueno (mentira)

--- src/app/pages/confirm-asistencia/confirm-asistencia.page.ts
+++ src/app/pages/confirm-asistencia/confirm-asistencia.page.ts
@@ -45,6 +45,7 @@ export class ConfirmAsistenciaPage implements OnInit {
       return;
     }
 
+    // Crear el objeto de asistencia
     const asistencia = {
       alumnoNombre: this.usuario.name || '',
       alumnoApellido: this.usuario.lastname || '',
